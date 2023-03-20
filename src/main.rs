8c8f0f000c052380fb6e080fd69379710948612f
arael <arael@monkeytype.com>
2023-03-20 12:13:27 -0400

    async reading

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 mod types;
 
-use std::{io::{self, stdout, Write}, thread::sleep, time::Duration};
+use std::{
+    io::{self, stdout, Read, Write},
+    thread::sleep,
+    time::Duration
+};
 use termion::{
     style,
     clear,
     color,
     terminal_size,
     cursor::{self, Hide, Show},
-    raw::IntoRawMode,
+    raw::IntoRawMode, async_stdin,
 };
 use types::Point;
 
@@ -24,19 +28,12 @@ const TRAILS: [[char; 4]; 4] = [
 pub const TURNCHANCE: u16 = 12;
 
 fn main() -> io::Result<()>{
-    // let stdout = stdout();
-    // let _ = stdout.into_raw_mode().unwrap();
-
-    // Cleanup when program executes. show cursor, clear screen
-    let _handler = ctrlc::set_handler(|| {
-        print!("{}{}{}{}", clear::All, style::Reset, Show, cursor::Goto(1, 1));
-        io::stdout().flush().expect("Failed to clean up.");
-        std::process::exit(0);
-    }).unwrap();
+    let stdout = stdout();
+    let mut stdout = stdout.into_raw_mode().unwrap();
+    let mut stdin = async_stdin().bytes();
 
     // Clear screen and hide cursor
-    print!("{}{}{}", clear::All, style::Bold, Hide);
-    io::stdout().flush()?;
+    write!(stdout, "{}{}{}", clear::All, style::Bold, Hide)?;
 
     // terminal bounds and vec of points
     let bounds:(u16, u16) = terminal_size().unwrap();
@@ -49,27 +46,41 @@ fn main() -> io::Result<()>{
 
     // event loop
     loop {
-        // Print and step point
-        let point = &mut pv[active as usize];
-        let ch = TRAILS[(&point.direction).get_u8() as usize]
-            [(&point.next_direction).get_u8() as usize];
-        print!(
-            "{}{}{}", // half block
-            cursor::Goto(point.pos.0, point.pos.1),
-            color::Fg(point.color.as_ref()),
-            ch
-        );
-        if point.step(&bounds) { active += 1; }
-        if active as u8 >= COUNT { active = 0; }
+        // if user presses 'q', end program
+        let b = stdin.next();
+        if let Some(Ok(b'q')) = b {
+            break;
+        }
 
         // Flush the output to the terminal
-        io::stdout().flush()?;
-        sleep(Duration::from_millis(30));
-        
-        n += 1;
-        // reset terminal after a certain number of prints
-        if n < s { continue; }
-        print!("{}", clear::All);
-        n = 0;
+        stdout.flush()?;
+
+         // Print and step point
+         let point = &mut pv[active as usize];
+         let ch = TRAILS[(&point.direction).get_u8() as usize]
+             [(&point.next_direction).get_u8() as usize];
+         write!(
+             stdout,
+             "{}{}{}",
+             cursor::Goto(point.pos.0, point.pos.1),
+             color::Fg(point.color.as_ref()),
+             ch
+         )?;
+         if point.step(&bounds) { active += 1; }
+         if active as u8 >= COUNT { active = 0; }
+         
+         // pause
+         sleep(Duration::from_millis(30));
+         n += 1;
+ 
+         // reset terminal after a certain number of prints
+         if n < s { continue; }
+         write!(stdout, "{}", clear::All)?;
+         n = 0;
     }
+
+    write!(stdout, "{}{}{}{}", clear::All, style::Reset, Show, cursor::Goto(1, 1))?;
+    stdout.flush().expect("Failed to clean up.");
+
+    Ok(())
 }
